1c00c588bd23efd29417374513bbb9c1485b833d
EonsofStupid <35156926+EonsofStupidity@users.noreply.github.com>
2025-01-27 20:34:43 -0500

    refractor profileedit for single logic

---
+++ src/components/profile/index.ts
@@ -0,0 +1,4 @@
+export { ProfileEditor } from "./ProfileEditor";
+export { ProfileForm } from "./ProfileForm";
+export { ProfilePicture } from "./ProfilePicture";
+export { ProfileDetails } from "./ProfileDetails"; 
