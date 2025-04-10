c55d0217cfce8a072d812d5844dc1036ab97a79a
Hades <jessay@gmail.com>
2025-04-09 17:51:13 -0700

    properly registering chat

--- src/chat/src/chat-client/index.ts
+++
@@ -1,7 +0,0 @@
-
-// Chat client module exports
-export * from './components/Chat';
-export * from './components/DockableChat';
-export * from './hooks/useChat';
-export * from './hooks/useChatSession';
-export * from './lib/ragClient';
