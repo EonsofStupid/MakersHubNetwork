2b60257b2cfe0895317306535eabd39caf1afa60
EonsofStupid <101021880+EonsofStupid@users.noreply.github.com>
2025-04-11 16:47:40 -0700

    Delete src/stores directory

--- src/stores/performance/selectors/memory.ts
+++
@@ -1,5 +0,0 @@
-import { PerformanceState } from '../types';
-
-export const selectMemoryMetrics = (state: PerformanceState) => state.metrics.memoryMetrics;
-export const selectHeapSize = (state: PerformanceState) => state.metrics.memoryMetrics.heapSize;
-export const selectInstances = (state: PerformanceState) => state.metrics.memoryMetrics.instances;
