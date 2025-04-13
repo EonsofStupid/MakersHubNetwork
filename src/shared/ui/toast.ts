0a00dc36ef72417e3f2c0bc611963261d1285b1d
EonsofStupid <101021880+EonsofStupid@users.noreply.github.com>
2025-04-13 14:57:53 -0700

    Delete src/shared/ui/toast.ts

--- src/shared/ui/toast.ts
+++
@@ -1,6 +0,0 @@
-
-import { Toast, ToastAction, ToastClose, ToastDescription, ToastProvider, ToastTitle, ToastViewport } from '@/components/ui/toast';
-import type { ToastActionElement, ToastProps, ToastVariant } from '@/components/ui/toast';
-
-export { Toast, ToastAction, ToastClose, ToastDescription, ToastProvider, ToastTitle, ToastViewport };
-export type { ToastActionElement, ToastProps, ToastVariant };
