67e1690795d7c83627697c6a6fe01f9d93651c6b
Hades <jessay@gmail.com>
2025-04-14 05:43:50 -0700

    resolving errors

--- src/app/layout/MainNav/components/AuthSection.tsx
+++ src/app/layout/MainNav/components/AuthSection.tsx
@@ -1,10 +1,9 @@
-
 import React from 'react';
 import { useAuthStore } from '@/auth/store/auth.store';
 import { UserMenu } from '@/app/components/auth/UserMenu';
 import { AuthSheet } from '@/app/components/auth/AuthSheet';
 import { RBACBridge } from '@/rbac/bridge';
-import { useLogger } from '@/logging/logger.service';
+import { useLogger } from '@/hooks/use-logger';
 import { LogCategory } from '@/shared/types/shared.types';
 
 const AuthSection: React.FC = () => {
